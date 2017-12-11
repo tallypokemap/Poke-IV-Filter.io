ba5bcf48e79f82050950c7494c8e9ca0a12dbc93
Jenner Palacios <japp_17@hotmail.com>
2017-12-10 18:25:51 -0800

    Load alarm.js function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var pokeNumbers = new Array("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22",
+var pokeNumbers=new Array("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22",
 						"23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43",
 						"44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64",
 						"65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85",
@@ -20,13 +20,13 @@ var pokeNumbers = new Array("1","2","3","4","5","6","7","8","9","10","11","12","
 						"358","359","360","361","362","363","364","365","366","367","368","369","370","371","372","373","374","375",
 						"376","377","378","379","380","381","382","383","384","385","386");
 
-var pokeCommons = new Array("10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","27","28","29","30","32","33",
+var pokeCommons=new Array("10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","27","28","29","30","32","33",
 						"39","40","41","42","46","47","48","49","50","51","52","53","54","55","81","82","84","85","96","97","98","99",
 						"100","101","104","105","109","110","118","119","120","121","161","162","163","164","165","166","167","168",
 						"170","171","177","178","183","184","187","188","189","190","193","194","195","198","200","202","203","206",
 						"207","220","221","223","224");
 						
-var pokeNames = new Array("Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise",
+var pokeNames=new Array("Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise",
 						"Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata",
 						"Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran♀",
 						"Nidorina","Nidoqueen","Nidoran♂","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales",
@@ -68,9 +68,9 @@ var pokeNames = new Array("Bulbasaur","Ivysaur","Venusaur","Charmander","Charmel
 // Off function
 //
 function off(el) {
-	var pokeNumb = document.getElementById(el);
-	var pokeDis = "p"+el;
-	pokeNumb.value = "OFF";
+	var pokeNumb=document.getElementById(el);
+	var pokeDis="p"+el;
+	pokeNumb.value="OFF";
 	document.getElementById(pokeDis).className="poke disabled";
 }
 
@@ -79,11 +79,11 @@ function off(el) {
 // ReEnable OFF'ed Pokemon
 //
 function reEna(el) {
-	var divID = "p"+el;
-	document.getElementById(divID).className = "poke";
-	document.getElementById(divID).style.display = "inline-block";
+	var divID="p"+el;
+	document.getElementById(divID).className="poke";
+	document.getElementById(divID).style.display="inline-block";
 	if(document.getElementById(el).value==="OFF") { 
-		document.getElementById(divID).className = "poke disabled";
+		document.getElementById(divID).className="poke disabled";
 	}
 }
 
@@ -93,18 +93,18 @@ function reEna(el) {
 // ################################################
 var ntoggled="no";
 function toggleNa() {
-	var x = document.getElementsByClassName("pname");
+	var x=document.getElementsByClassName("pname");
 	var i;
 	if(ntoggled==="no") {
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";
+		for (i=0; i < x.length; i++) {
+			x[i].style.display="none";
 		}
 	ntoggled="yes";
 	return;
 	}
 	if(ntoggled==="yes") {
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "block";
+		for (i=0; i < x.length; i++) {
+			x[i].style.display="block";
 		}
 	ntoggled="no";
 	return;
@@ -117,18 +117,18 @@ function toggleNa() {
 // ################################################
 var btoggled="no";
 function toggleBu() {
-	var x = document.getElementsByClassName("offbutton");
+	var x=document.getElementsByClassName("offbutton");
 	var i;
 	if(btoggled==="no") {
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";
+		for (i=0; i < x.length; i++) {
+			x[i].style.display="none";
 		}
 	btoggled="yes";
 	return;
 	}
 	if(btoggled==="yes") {
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "block";
+		for (i=0; i < x.length; i++) {
+			x[i].style.display="block";
 		}
 	btoggled="no";
 	return;
@@ -143,14 +143,14 @@ var showCo="yes";
 function toggleCo() {
 	var i;
 	if(showCo==="yes") {
-		for (i = 0; i < pokeCommons.length; i++) {
+		for (i=0; i < pokeCommons.length; i++) {
 			off(pokeCommons[i]);
 		}
 		showCo="no";
 		return
 	}
 	else {
-		for (i = 0; i < pokeCommons.length; i++) {
+		for (i=0; i < pokeCommons.length; i++) {
 			document.getElementById(pokeCommons[i]).value="98";
 			reEna(pokeCommons[i]);
 		}
@@ -166,17 +166,17 @@ var bCo="no";
 function toggleHiShoCo() {
 	var i;
 	if(bCo==="no") {
-		for (i = 0; i < pokeNumbers.length; i++) {
-			var minIV = document.getElementById(pokeNumbers[i]).value; 
-			if(minIV==="OFF") { document.getElementById("p"+pokeNumbers[i]).style.display = "none" }
+		for (i=0; i < pokeNumbers.length; i++) {
+			var minIV=document.getElementById(pokeNumbers[i]).value; 
+			if(minIV==="OFF") { document.getElementById("p"+pokeNumbers[i]).style.display="none" }
 		}
 	bCo="yes";
 	return;
 	}
 	if(bCo==="yes") {
-		for (i = 0; i < pokeNumbers.length; i++) {
-			var minIV = document.getElementById(pokeNumbers[i]).value; 
-			if(minIV==="OFF") { document.getElementById("p"+pokeNumbers[i]).style.display = "inline-block" }
+		for (i=0; i < pokeNumbers.length; i++) {
+			var minIV=document.getElementById(pokeNumbers[i]).value; 
+			if(minIV==="OFF") { document.getElementById("p"+pokeNumbers[i]).style.display="inline-block" }
 		}
 	bCo="no";
 	return;
@@ -188,21 +188,21 @@ function toggleHiShoCo() {
 //		Hide Legendaries and Regional Pokemon
 // ################################################
 var bUn="no";
-var pUn = new Array("p83","p115","p122","p132","p144","p145","p146","p150","p151","p172","p173","p174",
+var pUn=new Array("p83","p115","p122","p132","p144","p145","p146","p150","p151","p172","p173","p174",
 					"p175","p182","p186","p192","p196","p197","p199","p208","p212","p214","p222","p225","p225","p230","p233","p235",
 					"p236","p238","p239","p240","p243","p244","p245","p249","p250","p251");
 function toggleUn() {
 	var i;
 	if(bUn==="no") {
-		for (i = 0; i < pUn.length; i++) {
-			document.getElementById(pUn[i]).style.display = "none";
+		for (i=0; i < pUn.length; i++) {
+			document.getElementById(pUn[i]).style.display="none";
 		}
 	bUn="yes";
 	return;
 	}
 	if(bUn==="yes") {
-		for (i = 0; i < pUn.length; i++) {
-			document.getElementById(pUn[i]).style.display = "inline-block";
+		for (i=0; i < pUn.length; i++) {
+			document.getElementById(pUn[i]).style.display="inline-block";
 		}
 	bUn="no";
 	return;
@@ -221,18 +221,18 @@ function loadList() {
 	var i; bCo="no";
 	
 	// Separate by comma into array
-	var loadList = document.getElementById("loadlst").value; loadList=loadList.split(","); 
+	var loadList=document.getElementById("loadlst").value; loadList=loadList.split(","); 
 	// Clear spaces
-	for (i = 0; i < loadList.length; i++) {
+	for (i=0; i < loadList.length; i++) {
 		loadList[i]=loadList[i].replace(" ","");
 	}
 	// Disable and hide all
-	for (i = 0; i < pokeNumbers.length; i++) {
+	for (i=0; i < pokeNumbers.length; i++) {
 		off(pokeNumbers[i]);
 	}
 	toggleHiShoCo();
 	// Enable and Show
-	for (i = 0; i < loadList.length; i++) {
+	for (i=0; i < loadList.length; i++) {
 		document.getElementById(loadList[i]).value="0";
 		reEna(loadList[i]);
 	}
@@ -247,15 +247,15 @@ function loadList() {
 //		Webhook Filter Generator
 // #############################################
 function whook() {
-	var wht = document.getElementById("wh"); wht.value="";
-	for (i = 0; i < pokeNumbers.length; i++) {
-		var minIV = document.getElementById(pokeNumbers[i]).value; if(minIV==="OFF") { minIV="101" }
+	var wht=document.getElementById("wh"); wht.value="";
+	for (i=0; i < pokeNumbers.length; i++) {
+		var minIV=document.getElementById(pokeNumbers[i]).value; if(minIV==="OFF") { minIV="101" }
 		wht.value += pokeNumbers[i] + ": " + minIV + ",\n";
 	}
-	var newTxt = document.getElementById("wh");
-	paTxt = newTxt.value; 
-	paTxt = paTxt.slice(0, -2); 
-	newTxt.value = paTxt;
+	var newTxt=document.getElementById("wh");
+	paTxt=newTxt.value; 
+	paTxt=paTxt.slice(0, -2); 
+	newTxt.value=paTxt;
 }
 
 
@@ -263,19 +263,58 @@ function whook() {
 //		PokeAlarm Filter Generator
 // #############################################
 function palarm() {
-	var pat = document.getElementById("pa"); pat.value="";
-	for (i = 0; i < pokeNames.length; i++) {
-		var minIV = document.getElementById(pokeNumbers[i]).value;
-		var PAminIV = "\": {\"min_iv\":\"" + minIV + "\"},\n";
+	var pat=document.getElementById("pa"); pat.value="";
+	for (i=0; i < pokeNames.length; i++) {
+		var minIV=document.getElementById(pokeNumbers[i]).value;
+		var PAminIV="\": {\"min_iv\":\"" + minIV + "\"},\n";
 		if(minIV==="OFF") { PAminIV="\": \"False\",\n" }
 		if(minIV==="0") { PAminIV="\": \"True\",\n" }
 		// if(minIV<=90){if(minIV!=0){minIV=((minIV)*1+5); console.info("less than 90")}}
 		pat.value += "		\"" + pokeNames[i] + PAminIV;
 	}
-	var newTxt = document.getElementById("pa");
-	paTxt = newTxt.value; 
-	paTxt = paTxt.slice(0, -2); 
-	newTxt.value = paTxt;
+	var newTxt=document.getElementById("pa");
+	paTxt=newTxt.value; 
+	paTxt=paTxt.slice(0, -2); 
+	newTxt.value=paTxt;
+}
+
+
+// #############################################
+//		PokeAlarm Filter Loader
+// #############################################
+function lpalarm() {
+	var valStart="{"; var valEnd="}";
+	var pat=document.getElementById("pa");
+	if(pat.value.includes("pokemon") || pat.value.includes("raids")){return alert("Error: only pokemon!\nIE:\n\n\"Ivysaur\": {\"min_iv\":\"80\"},\n\"Venusaur\": {\"min_iv\":\"0\"},")}
+	
+	var jsonConvo=valStart+pat.value+valEnd; 
+	try { jsonConvo=JSON.parse(jsonConvo); }
+	catch(err) { return alert("Error reading your alarms.js...\nYou either have a comma at the end...\n"
+								+"or you have curly bracket at the beginning/end...\n\nPokemon only! IE:\n"
+								+"\"Charmander\": {\"min_iv\":\"80\", \"max_cp\": \"789\"},\n"
+								+"\"Charmeleon\": {\"min_iv\":\"50\"},\n\n[ERROR]:\n"+err); }
+	
+	var filterNames=Object.keys(jsonConvo); filterNames=filterNames.toString();filterNames=filterNames.split(",");
+	
+	var i; 
+	// Disable and hide all
+	for (i=0; i < pokeNumbers.length; i++) {
+		off(pokeNumbers[i]);
+	}
+	
+	// Enable and Show
+	for (i=0; i < filterNames.length; i++) {
+		if(jsonConvo[filterNames[i]].min_iv){
+			document.getElementById(pokeNumbers[pokeNames.indexOf(filterNames[i])]).value=jsonConvo[filterNames[i]].min_iv;
+			reEna(pokeNumbers[pokeNames.indexOf(filterNames[i])]);
+		}
+		if(jsonConvo[filterNames[i]]==="True"){
+			document.getElementById(pokeNumbers[pokeNames.indexOf(filterNames[i])]).value="0";
+			reEna(pokeNumbers[pokeNames.indexOf(filterNames[i])]);
+		}
+	}
+	bCo="no";
+	toggleHiShoCo();
 }
 
 
@@ -283,16 +322,16 @@ function palarm() {
 //		Black/White List Generator
 // #############################################
 function genBlk() {
-	var outputT = document.getElementById("genlst"); outputT.value=""; 
-	for (i = 0; i < pokeNumbers.length; i++) {
-		var minIV = document.getElementById(pokeNumbers[i]).value; 
+	var outputT=document.getElementById("genlst"); outputT.value=""; 
+	for (i=0; i < pokeNumbers.length; i++) {
+		var minIV=document.getElementById(pokeNumbers[i]).value; 
 		if(minIV==="OFF") { cast=pokeNumbers[i]+"," }if(minIV!="OFF") { cast="" }
 		outputT.value += cast;
 	}
-	var newTxt = document.getElementById("genlst");
-	genTxt = newTxt.value; 
-	genTxt = genTxt.slice(0, -1); 
-	newTxt.value = genTxt;
+	var newTxt=document.getElementById("genlst");
+	genTxt=newTxt.value; 
+	genTxt=genTxt.slice(0, -1); 
+	newTxt.value=genTxt;
 }
 
 
@@ -300,14 +339,14 @@ function genBlk() {
 //		White List Generator
 // #############################################
 function genWht() {
-	var outputT = document.getElementById("genlst"); outputT.value=""; 
-	for (i = 0; i < pokeNumbers.length; i++) {
-		var minIV = document.getElementById(pokeNumbers[i]).value; 
+	var outputT=document.getElementById("genlst"); outputT.value=""; 
+	for (i=0; i < pokeNumbers.length; i++) {
+		var minIV=document.getElementById(pokeNumbers[i]).value; 
 		if(minIV!="OFF") { cast=pokeNumbers[i]+"," }if(minIV==="OFF") { cast="" }
 		outputT.value += cast;
 	}
-	var newTxt = document.getElementById("genlst");
-	genTxt = newTxt.value; 
-	genTxt = genTxt.slice(0, -1); 
-	newTxt.value = genTxt;
+	var newTxt=document.getElementById("genlst");
+	genTxt=newTxt.value; 
+	genTxt=genTxt.slice(0, -1); 
+	newTxt.value=genTxt;
 }
